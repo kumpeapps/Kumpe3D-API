32a13f933c4c83714bbd64192326c42f5ac925bf
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 13:52:34 -0500

    -

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ api = Api(app)
 
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
-    response = jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR'], 'environ': request.environ})
+    response = jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR'], 'environ': request})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response, 201
 
