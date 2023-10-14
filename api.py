a8026ba2e6b924e178a2324e48db6c98e68c2d1e
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 15:11:09 -0500

    -

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ from flask import request
 from flask import jsonify
 from flask import Flask
 from flask_restful import Api
+from header_data import HeaderData
+import header_data
 
 # from flask_restful import Resource, Api, reqparse
 # import pandas as pd
@@ -12,17 +14,10 @@ api = Api(app)
 
 
 # get_my_ip functions
-@app.route("/get_my_ip", methods=["GET"])
-def get_my_ip_get():
-    """Displays User IP and referrer"""
-    response = jsonify(
-        {
-            "ip": request.environ["HTTP_X_FORWARDED_FOR"],
-            "referrer": request.environ["HTTP_REFERER"],
-        }
-    )
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+api.add_resource(header_data, "/headerdata")
+
+def get_product():
+    pass
 
 
 if __name__ == "__main__":
