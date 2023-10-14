9839389bf8b691ec787dceb498f488ab4259251e
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 14:45:50 -0500

    -

--- api.py
+++ api.py
@@ -1,16 +1,18 @@
 """REST API for Kumpe3D"""
 import setup # pylint: disable=unused-import, wrong-import-order
-import ast
 from flask import request
 from flask import jsonify
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
+from flask_restful import Api
+# from flask_restful import Resource, Api, reqparse
+# import pandas as pd
 app = Flask(__name__)
 api = Api(app)
 
+# get_my_ip functions
 @app.route("/get_my_ip", methods=["GET"])
-def get_my_ip():
+def get_my_ip_get():
+    """Displays User IP and referrer"""
     response = jsonify({
         'ip': request.environ['HTTP_X_FORWARDED_FOR'],
         'referrer': request.environ["HTTP_REFERER"]
