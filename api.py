262ef4a7f1d3439d14571c2f2955d40934d0e829
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 15:19:20 -0500

    -

--- api.py
+++ api.py
@@ -1,11 +1,8 @@
 """REST API for Kumpe3D"""
 import setup  # pylint: disable=unused-import, wrong-import-order
-from flask import request
-from flask import jsonify
 from flask import Flask
 from flask_restful import Api
-from header_data import HeaderData
-import header_data
+from header_data import HeaderData  # pylint: disable=no-name-in-module
 
 # from flask_restful import Resource, Api, reqparse
 # import pandas as pd
@@ -15,9 +12,6 @@ api = Api(app)
 
 # get_my_ip functions
 api.add_resource(HeaderData, "/headerdata")
-
-def get_product():
-    pass
 
 
 if __name__ == "__main__":
