b78e38b38a11a8dc0e9fb0379ed2a4dfbb1a0074
Justin Kumpe <jakumpe@kumpes.com>
2023-10-15 12:51:08 -0500

    -

--- products.py
+++ products.py
@@ -1,6 +1,6 @@
 """Products Function"""
 import setup  # pylint: disable=unused-import, wrong-import-order
-# from flask import request
+from flask import request
 from flask import jsonify
 from flask_restful import Resource
 from flask_restful import reqparse
