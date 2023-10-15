49e34daeeb510fdc1b40465ee0c3a3948621b1dc
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 19:15:20 -0500

    -

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import setup  # pylint: disable=unused-import, wrong-import-order
 from flask import Flask
 from flask_restful import Api
 from header_data import HeaderData  # pylint: disable=no-name-in-module
+from products import Product
 
 # from flask_restful import Resource, Api, reqparse
 # import pandas as pd
@@ -12,6 +13,7 @@ api = Api(app)
 
 # get_my_ip functions
 api.add_resource(HeaderData, "/headerdata")
+api.add_resource(Product, "/product")
 
 
 if __name__ == "__main__":
