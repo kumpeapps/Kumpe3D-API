eb43f5ab6cbc4a29a0bcfa26eee20ce4fab9b595
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 19:44:13 -0500

    -

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import setup  # pylint: disable=unused-import, wrong-import-order
 from flask import Flask
 from flask_restful import Api
 from header_data import HeaderData  # pylint: disable=no-name-in-module
-from products import Product
+# from products import Product
 
 # from flask_restful import Resource, Api, reqparse
 # import pandas as pd
