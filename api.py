3cfab7602792b0ad057f3606e9945d9b4b82c331
Justin Kumpe <jakumpe@kumpes.com>
2023-10-15 11:03:30 -0500

    -

--- api.py
+++ api.py
@@ -3,19 +3,32 @@ import setup  # pylint: disable=unused-import, wrong-import-order
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
-from header_data import HeaderData  # pylint: disable=no-name-in-module
+# from header_data import HeaderData  # pylint: disable=no-name-in-module
 # from products import Product
 
 # from flask_restful import Resource, Api, reqparse
 # import pandas as pd
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 # get_my_ip functions
-api.add_resource(HeaderData, "/headerdata")
+# api.add_resource(HeaderData, "/headerdata")
 
-# api.add_resource(Product, "/product")
+
+# api.add_resource(Product, "/headerdata")
+@app.route('/headerdata')
+def headerdata():
+    """Displays User IP and referrer"""
+    response = jsonify(
+        {
+            "ip": request.environ["HTTP_X_FORWARDED_FOR"],
+            "referrer": request.environ["HTTP_REFERER"],
+        }
+    )
+    return response, 201, {"Access-Control-Allow-Origin": "*"}
 
 
 if __name__ == "__main__":
