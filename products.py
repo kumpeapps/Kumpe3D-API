f472761bbe839d6bd596174b5cd91650bd63add9
Justin Kumpe <jakumpe@kumpes.com>
2023-10-15 13:58:23 -0500

    -

--- products.py
+++ products.py
@@ -23,17 +23,17 @@ class Product(Resource):
 
     def get(self):
         """Get Product Data"""
-        db = self.db
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument("sku", required=True)  # add required argument
-        args = parser.parse_args()  # parse arguments to dictionary
-        sku = helpers.get_sku_array(args["sku"])
-        cursor = db.cursor(pymysql.cursors.DictCursor)
-        sql = "CALL get_products(%s, %s, %s)"
-        cursor.execute(sql, (sku["base_sku"],'%', '%'))
-        response = cursor.fetchone()
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        response.mimetype = "application/json"
-        cursor.close()
-        db.close()
+        # db = self.db
+        # parser = reqparse.RequestParser()  # initialize
+        # parser.add_argument("sku", required=True)  # add required argument
+        # args = parser.parse_args()  # parse arguments to dictionary
+        # sku = helpers.get_sku_array(args["sku"])
+        # cursor = db.cursor(pymysql.cursors.DictCursor)
+        # sql = "CALL get_products(%s, %s, %s)"
+        # cursor.execute(sql, (sku["base_sku"],'%', '%'))
+        # response = cursor.fetchone()
+        # response.headers.add("Access-Control-Allow-Origin", "*")
+        # response.mimetype = "application/json"
+        # cursor.close()
+        # db.close()
         return {"test": 0}, 200, {"Access-Control-Allow-Origin": "*"}
