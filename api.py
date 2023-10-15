d993103ef283a2383046663edea74b958abc3303
Justin Kumpe <jakumpe@kumpes.com>
2023-10-14 20:23:33 -0500

    -

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 # import pandas as pd
 app = Flask(__name__)
 api = Api(app)
-cors = CORS(app, resources={r"/headerdata/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/headerdata": {"origins": "*"}})
 
 
 # get_my_ip functions
