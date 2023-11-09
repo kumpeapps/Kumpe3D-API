d03f1d32076c8f6d8fda909527b68d6ef40c10ad
Justin Kumpe <jakumpe@kumpes.com>
2023-11-09 16:43:43 -0600

    -

--- notif.py
+++ notif.py
@@ -1,5 +1,5 @@
 """Notification Scripts"""
-import setup # pylint: disable=unused-import, wrong-import-order
+import setup  # pylint: disable=unused-import, wrong-import-order
 import http.client
 import urllib
 from params import Params
@@ -8,14 +8,20 @@ from params import Params
 def new_order(order_number: int):
     """Send New Order Notification"""
     conn = http.client.HTTPSConnection(Params.Pushover.server)
-    conn.request("POST", "/1/messages.json",
-    urllib.parse.urlencode({
-        "token": Params.Pushover.apikey,
-        "user": Params.Pushover.orders_group,
-        "title": f"New Kumpe3D Order {order_number}",
-        "message": f"A new order has been submitted. The order number is {order_number}.",
-        "url": f"{Params.base_url}/packing_slip?order_id={order_number}",
-        "sound": "aol"
-    }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.request(
+        "POST",
+        "/1/messages.json",
+        urllib.parse.urlencode(
+            {
+                "token": Params.Pushover.apikey,
+                "user": Params.Pushover.orders_group,
+                "title": f"New Kumpe3D Order {order_number}",
+                "message": f"A new order has been submitted. The order number is {order_number}.",
+                "url": f"{Params.base_url}/packing_slip?order_id={order_number}",
+                "url_title": "View Packing Slip",
+                "sound": "aol",
+            }
+        ),
+        {"Content-type": "application/x-www-form-urlencoded"},
+    )
     conn.getresponse()
-new_order("32")
