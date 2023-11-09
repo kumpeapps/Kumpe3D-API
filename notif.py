0c14d3f086ab2628ca12ee1239fb2c05d50690de
Justin Kumpe <jakumpe@kumpes.com>
2023-11-09 16:32:43 -0600

    -

---
+++ notif.py
@@ -0,0 +1,20 @@
+"""Notification Scripts"""
+import setup # pylint: disable=unused-import, wrong-import-order
+import http.client
+import urllib
+from params import Params
+
+
+def new_order(order_number: int):
+    """Send New Order Notification"""
+    conn = http.client.HTTPSConnection(Params.Pushover.server)
+    conn.request("POST", "/1/messages.json",
+    urllib.parse.urlencode({
+        "token": Params.Pushover.apikey,
+        "user": Params.Pushover.orders_group,
+        "title": f"New Kumpe3D Order {order_number}",
+        "message": f"A new order has been submitted. The order number is {order_number}.",
+        "sound": "aol"
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
+new_order("0")
--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 """Parameters file for Kumpe3D API"""
-import setup # pylint: disable=unused-import, wrong-import-order
+import setup  # pylint: disable=unused-import, wrong-import-order
 import os
 from dotenv import load_dotenv
 import infisical
@@ -7,60 +7,112 @@ import logging
 
 
 load_dotenv()
-service_token = os.getenv('SERVICE_TOKEN')
-app_env = os.getenv('APP_ENV')
-creds = infisical.InfisicalClient(token=service_token, site_url='https://creds.kumpeapps.com')
+service_token = os.getenv("SERVICE_TOKEN")
+app_env = os.getenv("APP_ENV")
+creds = infisical.InfisicalClient(
+    token=service_token, site_url="https://creds.kumpeapps.com"
+)
+
 
 class Params:
     """Parameters"""
+
     base_url = creds.get_secret("URL", environment=app_env, path="/WEB/").secret_value
-    app_env = os.getenv('APP_ENV')
-    def log_level(): # pylint: disable=no-method-argument
+    app_env = os.getenv("APP_ENV")
+
+    def log_level():  # pylint: disable=no-method-argument
         """Returns Log Level"""
-        if os.getenv('LOG_LEVEL') == "info":
+        if os.getenv("LOG_LEVEL") == "info":
             return logging.INFO
-        elif os.getenv('LOG_LEVEL') == "warning":
+        elif os.getenv("LOG_LEVEL") == "warning":
             return logging.WARNING
-        elif os.getenv('LOG_LEVEL') == "error":
+        elif os.getenv("LOG_LEVEL") == "error":
             return logging.ERROR
-        elif os.getenv('LOG_LEVEL') == "debug":
+        elif os.getenv("LOG_LEVEL") == "debug":
             return logging.DEBUG
-        elif os.getenv('LOG_LEVEL') == "critical":
+        elif os.getenv("LOG_LEVEL") == "critical":
             return logging.CRITICAL
         else:
             return logging.INFO
+
     class SQL:
         """SQL Parameters for Web_3d User"""
-        username = creds.get_secret("USERNAME", environment=app_env, path="/MYSQL/").secret_value
-        password = creds.get_secret("PASSWORD", environment=app_env, path="/MYSQL/").secret_value
-        server = creds.get_secret("SERVER", environment=app_env, path="/MYSQL/").secret_value
-        port = creds.get_secret("PORT", environment=app_env, path="/MYSQL/").secret_value
-        database = creds.get_secret("DATABASE", environment=app_env, path="/MYSQL/").secret_value
 
-        def dict(): # pylint: disable=no-method-argument
+        username = creds.get_secret(
+            "USERNAME", environment=app_env, path="/MYSQL/"
+        ).secret_value
+        password = creds.get_secret(
+            "PASSWORD", environment=app_env, path="/MYSQL/"
+        ).secret_value
+        server = creds.get_secret(
+            "SERVER", environment=app_env, path="/MYSQL/"
+        ).secret_value
+        port = creds.get_secret(
+            "PORT", environment=app_env, path="/MYSQL/"
+        ).secret_value
+        database = creds.get_secret(
+            "DATABASE", environment=app_env, path="/MYSQL/"
+        ).secret_value
+
+        def dict():  # pylint: disable=no-method-argument
             """returns as dictionary"""
             return {
-                'user': Params.SQL.username,
-                'passwd': Params.SQL.password,
-                'host': Params.SQL.server,
-                'port': Params.SQL.port,
-                'db': Params.SQL.database
+                "user": Params.SQL.username,
+                "passwd": Params.SQL.password,
+                "host": Params.SQL.server,
+                "port": Params.SQL.port,
+                "db": Params.SQL.database,
             }
+
     class PayPal:
         """PayPal Parameters"""
-        api_url = creds.get_secret("API_URL", environment=app_env, path="/PAYPAL/").secret_value
-        client_id = creds.get_secret("CLIENT_ID", environment=app_env, path="/PAYPAL/").secret_value
-        secret = creds.get_secret("SECRET", environment=app_env, path="/PAYPAL/").secret_value
+
+        api_url = creds.get_secret(
+            "API_URL", environment=app_env, path="/PAYPAL/"
+        ).secret_value
+        client_id = creds.get_secret(
+            "CLIENT_ID", environment=app_env, path="/PAYPAL/"
+        ).secret_value
+        secret = creds.get_secret(
+            "SECRET", environment=app_env, path="/PAYPAL/"
+        ).secret_value
+
     class SendPulse:
         """Send Pulse Parameters"""
-        server = creds.get_secret("SERVER", environment=app_env, path="/SENDPULSE/").secret_value
-        port = creds.get_secret("PORT", environment=app_env, path="/SENDPULSE/").secret_value
-        username = creds.get_secret("USERNAME", environment=app_env, path="/SENDPULSE/").secret_value
-        password = creds.get_secret("PASSWORD", environment=app_env, path="/SENDPULSE/").secret_value
-        sender = creds.get_secret("SENDER", environment=app_env, path="/SENDPULSE/").secret_value
+
+        server = creds.get_secret(
+            "SERVER", environment=app_env, path="/SENDPULSE/"
+        ).secret_value
+        port = creds.get_secret(
+            "PORT", environment=app_env, path="/SENDPULSE/"
+        ).secret_value
+        username = creds.get_secret(
+            "USERNAME", environment=app_env, path="/SENDPULSE/"
+        ).secret_value
+        password = creds.get_secret(
+            "PASSWORD", environment=app_env, path="/SENDPULSE/"
+        ).secret_value
+        sender = creds.get_secret(
+            "SENDER", environment=app_env, path="/SENDPULSE/"
+        ).secret_value
+
+    class Pushover:
+        """Pushover API Parameters"""
+
+        apikey = creds.get_secret(
+            "APIKEY", environment=app_env, path="/PUSHOVER/"
+        ).secret_value
+        orders_group = creds.get_secret(
+            "ORDERS_GROUP", environment=app_env, path="/PUSHOVER/"
+        ).secret_value
+        server = creds.get_secret(
+            "SERVER", environment=app_env, path="/PUSHOVER/"
+        ).secret_value
 
 
 if __name__ == "__main__":
-    print("""Error: This file is a module to be imported and has no functions
-          to be ran directly.""")
+    print(
+        """Error: This file is a module to be imported and has no functions
+          to be ran directly."""
+    )
     print(Params.Email.username)
