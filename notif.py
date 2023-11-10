91e3d1b160c18b883740923dca49b9041861547a
Justin Kumpe <jakumpe@kumpes.com>
2023-11-10 16:07:53 -0600

    -

--- notif.py
+++ notif.py
@@ -1,14 +1,12 @@
 """Notification Scripts"""
 import setup  # pylint: disable=unused-import, wrong-import-order
 import http.client
-import requests
 import urllib
 from params import Params
 
 
 def new_order(order_number: int):
     """Send New Order Notification"""
-    packing_slip = get_packing_slip(order_number)
     conn = http.client.HTTPSConnection(Params.Pushover.server)
     conn.request(
         "POST",
@@ -18,9 +16,8 @@ def new_order(order_number: int):
                 "token": Params.Pushover.apikey,
                 "user": Params.Pushover.orders_group,
                 "title": f"New Kumpe3D Order {order_number}",
-                "message": packing_slip,
+                "message": f"A new order has been submitted. The order number is {order_number}.",
                 "url": f"{Params.base_url}/packing_slip?order_id={order_number}",
-                "html": 1,
                 "url_title": "View Packing Slip",
                 "sound": "aol",
             }
@@ -28,8 +25,3 @@ def new_order(order_number: int):
         {"Content-type": "application/x-www-form-urlencoded"},
     )
     conn.getresponse()
-
-def get_packing_slip(order_number: int):
-    url = f"{Params.base_url}/packing_slip?order_id={order_number}"
-    r = requests.get(url, timeout= 30)
-    return r.content
