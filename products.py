8b5439275bc6ee03522f38553c1363c31d9bdba5
Justin Kumpe <jakumpe@kumpes.com>
2023-11-18 22:21:05 -0600

    -

--- products.py
+++ products.py
@@ -42,8 +42,8 @@ class Product(Resource):
             )
         else:
             return (
-                {"response": response, "status_code": 204},
-                204,
+                {"response": [], "status_code": 200},
+                200,
                 {"Access-Control-Allow-Origin": Params.base_url},
             )
 
